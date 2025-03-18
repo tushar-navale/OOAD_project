7afe079bf54f06d5854477c05cf25c0f5f335c5c
tushar-navale <tushar29ns@gmail.com>
2025-03-18 19:21:20 +0530

    SpringBoot Setup

---
+++ FitnessTracker/src/test/java/com/example/FitnessTracker/FitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FitnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
